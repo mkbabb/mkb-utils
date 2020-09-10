955e66b8a6ba9e77ee6a16eee41161a923940b8d
Mike Babb <mike7400@gmail.com>
2020-09-10 03:10:36 -0400

    clone_db now accepts an optional sql dump.

--- src/clone_db.py
+++ src/clone_db.py
@@ -7,6 +7,8 @@ from typing import *
 
 from utils import quote_value, run
 
+import tempfile
+
 
 def create_connection_string(
     username: str, hostname: str, port: str = "3306", password: str = ""
@@ -38,7 +40,16 @@ def main() -> None:
         quote_value(to_db["database"], '"'),
     )
 
-    tables = " ".join(map(quote_value, from_db["tables"]))
+    tables = " ".join(map(quote_value, from_db.get("tables", [])))
+    dump_name = from_db.get("dump_name")
+    no_provided_dump = dump_name is None
+
+    if no_provided_dump:
+        dump_name = next(tempfile._get_candidate_names())
+        run(
+            f"mysqldump -f --single-transaction {from_connection_string} {from_db_name} {tables} > {dump_name}"
+        )
+        print(f"Dumped database {from_db_name}.")
 
     run(
         f"mysql {to_connection_string} "
@@ -47,11 +58,11 @@ def main() -> None:
 
     print(f"Created database {to_db_name}")
 
-    run(
-        f"mysqldump --single-transaction {from_connection_string} {from_db_name} {tables} | mysql {to_connection_string} {to_db_name}"
-    )
+    run(f"mysql {to_connection_string} {to_db_name} < {dump_name}")
 
-    print(f"Dumped tables: {tables}")
+    if no_provided_dump:
+        run(f"rm {dump_name}")
+        print(f"Removed temporary dump of {dump_name}")
 
 
 if __name__ == "__main__":
